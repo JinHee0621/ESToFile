d6e347ac984c5f3f552a16c5101d490783eed15e
HanJinHee <HanJinHee@DESKTOP-RLIBFHJ.mshome.net>
2021-06-06 13:15:27 +0900

    Update Graphic Ui
    2021-06-06
    
    Add Progress Bar and Alert Label and Etc..

--- testSparkApp/src/main/java/gui/GraphicUi.java
+++ testSparkApp/src/main/java/gui/GraphicUi.java
@@ -1,35 +1,38 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingWorker;
 
 import testSparkApp.elasticJoin;
 
 public class GraphicUi extends JFrame implements ActionListener{
 	
 	JTextField host = new JTextField(35);
-	JButton b1 = new JButton("실행");
+	public JButton b1 = new JButton("실행");
 	JTextArea query = new JTextArea(10,35);
 	Image title = null;
+	public JProgressBar progress = new JProgressBar();
+	public JLabel resultText = new JLabel();
+	public boolean isRunning = false;
 	
 	public GraphicUi() {
 		
 		try {
-			//File titleImage = new File("Resources/SparkEstoFileTitle.png");
 			title = Toolkit.getDefaultToolkit().getImage(getClass().getResource("Resources/SparkEstoFileTitle.png"));
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -41,7 +44,7 @@ public class GraphicUi extends JFrame implements ActionListener{
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		setLayout(new FlowLayout(FlowLayout.CENTER, 80, 20));
+		setLayout(new FlowLayout(FlowLayout.CENTER, 180, 20));
 		Container thisCon = getContentPane();
 		
 /*	    // 프레임(자바 화면) 크기
@@ -51,16 +54,23 @@ public class GraphicUi extends JFrame implements ActionListener{
 	    // (모니터화면 가로 - 프레임화면 가로) / 2, (모니터화면 세로 - 프레임화면 세로) / 2
 	    setLocation((screenSize.width - frameSize.width) /2, (screenSize.height - frameSize.height) /2);
 */
+		
+		b1.setText("실행");
+		b1.setForeground(new Color(0,0,0));
+		b1.setBackground(new Color(38,136,225));
+		
 		thisCon.add(new JLabel(new ImageIcon(title)));
 		thisCon.add(new JLabel("Elastic Host"));
 		thisCon.add(host);
 		thisCon.add(new JLabel("Query"));
 		thisCon.add(new JScrollPane(query));
 		thisCon.add(b1);
-		
 		b1.addActionListener(this);
 		
-		setSize(450,600);
+		thisCon.add(progress);
+		thisCon.add(resultText);
+		
+		setSize(450,700);
 		setVisible(true);
 	}
 	public static void main(String[] args) {
@@ -69,8 +79,20 @@ public class GraphicUi extends JFrame implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		elasticJoin join = new elasticJoin();
+		if(!isRunning) {
+			isRunning = true;
+			elasticJoin join = new elasticJoin(this);
+			
+	        final SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+	            @Override
+	            protected Void doInBackground() throws Exception {
+	            	join.runEsToFile(19850);
+	                progress.setValue(0);
+	                return null;
+	            }
+	        };
+	        worker.execute();
+		}
 	}
 
 }
--- testSparkApp/src/main/java/testSparkApp/elasticJoin.java
+++ testSparkApp/src/main/java/testSparkApp/elasticJoin.java
@@ -1,18 +1,55 @@
 package testSparkApp;
 
-import java.util.List;
+import java.awt.Color;
 
-import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 import org.elasticsearch.spark.sql.api.java.JavaEsSparkSQL;
 
+import gui.GraphicUi;
+
 public class elasticJoin {
-  
-	public elasticJoin() {
-		System.out.println("조인 실행");
+	
+	public GraphicUi gui; 
+	
+	public elasticJoin(GraphicUi gui) {
+		this.gui = gui;
+	}
+	
+	public void runEsToFile(int size) {
+		try {
+			gui.b1.setText("실행 중");
+			gui.b1.setForeground(new Color(255,255,255));
+			gui.b1.setBackground(new Color(57,82,140));
+			int progressVal = 1;
+			
+			//ProgressBar Update
+			//---------------------------------------------------
+			for(int i = 0; i < size; i += (size/100)) {
+				gui.progress.setValue(progressVal++);
+		        Thread.sleep(20);
+			}
+			progressVal = 0;
+			//---------------------------------------------------
+			//throw new Exception();
+			gui.b1.setText("실행");
+			gui.b1.setForeground(new Color(0,0,0));
+			gui.b1.setBackground(new Color(38,136,225));
+			gui.resultText.setForeground(Color.black);
+			gui.resultText.setText("파일 생성을 성공적으로 완료했습니다.");
+			gui.isRunning = false;
+			
+		} catch (Exception e) {
+			gui.b1.setText("실행");
+			gui.b1.setForeground(new Color(0,0,0));
+			gui.b1.setBackground(new Color(38,136,225));
+			gui.resultText.setText("오류가 발생하여 파일 생성에 실패하였습니다.");
+			gui.resultText.setForeground(Color.red);
+			gui.isRunning = false;
+		}
+		
 	}
 	
 	public static void main(String[] args) {
@@ -20,7 +57,7 @@ public class elasticJoin {
 		
 		SparkSession sc = SparkSession
 			      .builder()
-			      //.config("spark.master", "spark://192.168.0.101:7077")
+			      .config("spark.master", "spark://192.168.0.101:7077")
 			      .config("es.nodes", "http://192.168.0.101:9200")
 			      .config("spark.sql.shuffle.partitions", 10)
 			      .appName("jointest")
